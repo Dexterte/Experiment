738d7786df2dbd6664830607bb15b9efe35d83b9
Dexterte <rejimully@gmail.com>
2023-09-18 16:35:25 +0900

    TaskテーブルのDDL追加

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,3 +4,12 @@ name nvarchar(255) not null,
 age int not null
 )
 
+
+use learing_spring;
+CREATE TABLE Task(
+task_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+title nvarchar(100) NOT NULL,
+content nvarchar(255) NOT NULL,
+created_at datetime DEFAULT CURRENT_TIMESTAMP,
+updated_at datetime 
+)
